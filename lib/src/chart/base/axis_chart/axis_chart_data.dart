993608e5580751c671e2bde4decdc20b6d3f6a42
Babak Baradari <babak.baradari93@gmail.com>
2024-08-08 01:00:10 +0330

    Add image class to dot painters class

--- lib/src/chart/base/axis_chart/axis_chart_data.dart
+++ lib/src/chart/base/axis_chart/axis_chart_data.dart
@@ -1621,3 +1621,108 @@ class FlDotCrossPainter extends FlDotPainter {
         width,
       ];
 }
+
+/// This class is an implementation of a [FlDotPainter] that draws
+/// a image shape
+class FlDotImagePainter extends FlDotPainter {
+  /// The color of the image is determined determined by [color],
+  /// [radius] determines the radius of the image.
+  /// You can have a stroke line around the image,
+  /// by setting the thickness with [strokeWidth],
+  /// and you can change the color of of the stroke with [strokeColor].
+  FlDotImagePainter(this.image, {
+    this.color = Colors.green,
+    double? radius,
+    this.strokeColor = const Color.fromRGBO(76, 175, 80, 1),
+    this.strokeWidth = 0.0,
+  }) : radius = radius ?? 4.0;
+
+  /// The fill color to use for the circle
+  Color color;
+
+  /// Customizes the radius of the circle
+  double radius;
+
+  /// The stroke color to use for the circle
+  Color strokeColor;
+
+  /// The stroke width to use for the circle
+  double strokeWidth;
+
+  /// Is the showing image.
+  Image image;
+
+  /// Implementation of the parent class to draw the circle
+  @override
+  void draw(Canvas canvas, FlSpot spot, Offset offsetInCanvas) {
+    canvas.drawImage(image, offsetInCanvas, Paint());
+    if (strokeWidth != 0.0 && strokeColor.opacity != 0.0) {
+      canvas.drawCircle(
+        offsetInCanvas,
+        radius + (strokeWidth / 2),
+        Paint()
+          ..color = strokeColor
+          ..strokeWidth = strokeWidth
+          ..style = PaintingStyle.stroke,
+      );
+    }
+    canvas.drawCircle(
+      offsetInCanvas,
+      radius,
+      Paint()
+        ..color = color
+        ..style = PaintingStyle.fill,
+    );
+  }
+
+  /// Implementation of the parent class to get the size of the circle
+  @override
+  Size getSize(FlSpot spot) {
+    return Size(radius * 2, radius * 2);
+  }
+
+  @override
+  Color get mainColor => color;
+
+  FlDotCirclePainter _lerp(
+      FlDotCirclePainter a,
+      FlDotCirclePainter b,
+      double t,
+      ) {
+    return FlDotCirclePainter(
+      color: Color.lerp(a.color, b.color, t)!,
+      radius: lerpDouble(a.radius, b.radius, t),
+      strokeColor: Color.lerp(a.strokeColor, b.strokeColor, t)!,
+      strokeWidth: lerpDouble(a.strokeWidth, b.strokeWidth, t)!,
+    );
+  }
+
+  @override
+  FlDotPainter lerp(FlDotPainter a, FlDotPainter b, double t) {
+    if (a is! FlDotCirclePainter || b is! FlDotCirclePainter) {
+      return b;
+    }
+    return _lerp(a, b, t);
+  }
+
+  @override
+  bool hitTest(
+      FlSpot spot,
+      Offset touched,
+      Offset center,
+      double extraThreshold,
+      ) {
+    final distance = (touched - center).distance.abs();
+    return distance < radius + extraThreshold;
+  }
+
+  /// Used for equality check, see [EquatableMixin].
+  @override
+  List<Object?> get props => [
+    color,
+    radius,
+    strokeColor,
+    strokeWidth,
+    image,
+  ];
+}
